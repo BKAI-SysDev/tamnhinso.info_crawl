59d686c1f89bd4bf8708e4df82a4d211dda4d69a
KaynAssassin <allmid66@gmail.com>
2019-08-03 07:05:43 +0700

    update

--- tamnhinso/spiders/phim_bo.py
+++ tamnhinso/spiders/phim_bo.py
@@ -5,6 +5,7 @@ from scrapy.shell import inspect_response
 from scrapy.linkextractors import LinkExtractor
 from w3lib.html import remove_tags
 from scrapy_splash import SplashRequest
+import re
 
 
 class TnsSpider(scrapy.Spider):
@@ -23,6 +24,8 @@ class TnsSpider(scrapy.Spider):
                     return splash:html()
                 end
             """
+
+    link_films = dict()
 
     def start_requests(self):
         for url in self.start_urls:
@@ -45,12 +48,30 @@ class TnsSpider(scrapy.Spider):
         result["intro"] = intro
         btn = btn[0].strip()
         if(btn == 'XEM PHIM'):
+            # episode_btn = list(map(remove_tags,response.xpath("//div[@class = 'col-md-6']/ul[@class = 'chap-list']/li/a").getall()))
+            # episode_btn = list(map(lambda x:x.strip(),episode_btn))   
+            # link_films = dict()
+            for x in episode_btn:
+                self.link_films[x] = 0                                                                                                                                 
             video_btn = LinkExtractor(restrict_xpaths="//div[@class = 'col-md-6']/ul[@class = 'chap-list']/li").extract_links(response)
             for x in video_btn:
-                yield SplashRequest(url= x.url,callback = self.parse_link_film,endpoint = 'execute',args={'lua_source': self.script,'wait':5,'timeout':3600},meta = {"item":result})
-
+                yield SplashRequest(url= x.url,callback = self.parse_link_film,endpoint = 'execute',args={'lua_source': self.script,'wait':8,'timeout':3600},meta = {"list_links":link_films,"url":x.url})
         else:
            yield None
-           
 
-       
+    def parse_link_film(self,response):
+        list_links = response.meta.get("list_links")
+        url = response.meta.get("url")
+        pattern=re.compile(r'(?<=tap-)(\d+)')
+        episode = pattern.findall(url)
+        self.link_films[episode] = self.get_link(response)
+
+    def get_link(self,response):
+        link = dict()
+        link_720 = response.xpath("//source[@label = '720']/@src").getall()
+        link_360 = response.xpath("//source[@label = '360']/@src").getall()
+        link["360p"] = link_360
+        link["720p"] = link_720
+        return link
+    
+
